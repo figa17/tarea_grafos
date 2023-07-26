cf5e047dd533513ef6ab64e9c12b47f4238d8a69
Felipe González Alfaro <figgonzalez@falabella.cl>
2023-07-26 18:19:28 -0400

    add path

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ def main():
     n_nodes = len(distance_matrix)
     graph = Graph(n_nodes)
     for index, row in distance_matrix.iterrows():
-        first_node = int(index) + 1
-        for i in range(first_node, n_nodes):
-            if row[i] < 16:
+        for i in range(n_nodes):
+            if i != index and row[i] < 16:
                 graph.add_edge(index, i, row[i])
 
     graph.print_adj_list()
+    print(graph.bfs(0, 15))
+    print(graph.dfs(0, 15))
+
 
 if __name__ == '__main__':
     main()
