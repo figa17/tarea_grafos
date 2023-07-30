d16e4c3e7c457ae444c2c6936fc944287b2c2a9e
Tato0069 <matias.zapata.barra@gmail.com>
2023-07-30 19:54:17 -0400

    chore: change logs

--- main_dummy.py
+++ main_dummy.py
@@ -1,14 +1,14 @@
 from dummy_graph import RandomGraph
 
 def main():
-    print('creating tree')
 
     dummyGraph = RandomGraph(10, directed=False)
     dummyGraph.view_graph()
     data_dfs = dummyGraph.dfs()
-    print(data_dfs)
+    print(f"Resultado Busqueda por Profundidad: {data_dfs}")
     data_bfs = dummyGraph.bfs()
-    print(data_bfs)
+    print()
+    print(f"Resultado Busqueda por Anchura: {data_bfs}")
 
 
 if __name__ == '__main__':
